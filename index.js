c81b826a81a0bc516619ca77bfd13f993c5eb23e
Mo Al <malbazaz@mba2018.hbs.edu>
2018-08-22 14:04:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,6 +15,6 @@ return {firstName: full[0], lastName: full[1]}
 function attributesToPhrase(drivers){
 
 return drivers.map(function(d){
-    return `${d.name} is from ${d['hometown']`;
+    return `${d.name} is from ${d['hometown']`
   })
 }
